43486ccde57f766152cfabfccc7eb6f031bed169
Dan G <datadan_1@yahoo.com>
2015-07-20 21:45:57 -0700

    First attempt at editing cachematrix to speed up cashing of inverse matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,21 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-
+  #Set the value of the matrix
+  #get the value of the matrix
+  #set the value of the matrix inverse solve(x)
+  #get the value of the matrix inverse
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y){
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- mean
+    getsolve <- function() m
+    list(set = set, get = get, setsolve=setsolve,getsolve=getsolve)
 }
 
 
@@ -12,4 +23,14 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setsolve(m)
+    m
 }
+
